90ab77afd731e637cc872e479bcbbd8624c4b0dc
Pedro Mendes <pedrohh0012@gmail.com>
2025-05-03 17:00:07 -0300

    feat: :construction: Estruturação inicial da página de Login
    Designer e estrutura inicial do layout da tela de login, podendo sofrer alterações durante o processo.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,31 @@
+import { Input, Button } from "antd";
+import { GoogleOutlined } from "@ant-design/icons";
+
 function Login() {
-  return <h1>Login</h1>;
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-50">
+      <section className="w-[320px] rounded-md border border-gray-400 bg-white p-6 shadow-md">
+        <h1 className="mb-4 text-center text-2xl font-semibold">Login</h1>
+        <form className="flex flex-col gap-4">
+          <Input placeholder="Email" type="email" />
+          <Input.Password placeholder="Senha" />
+          <Button type="primary" htmlType="submit" className="w-full">
+            Entrar
+          </Button>
+        </form>
+
+        <div className="my-4 flex items-center gap-2">
+          <hr className="flex-grow border-t text-gray-400" />
+          <span className="text-sm text-gray-500">ou</span>
+          <hr className="flex-grow border-t text-gray-400" />
+        </div>
+
+        <Button icon={<GoogleOutlined />} className="w-full" type="default">
+          Entrar com Google
+        </Button>
+      </section>
+    </main>
+  );
 }
 
 export default Login;
