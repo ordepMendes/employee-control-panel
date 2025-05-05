d5dca8b55670d6d517ea70764aef6d1b2f3037f9
Pedro Mendes <pedrohh0012@gmail.com>
2025-05-05 18:35:20 -0300

    fix: :bug: Persistencia de usuário caso ja esteja logado
    Agora o usuario e mantido na sessao até expirar o login ou dar logout, resolvido o problema de falta de persistencia

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -4,23 +4,31 @@ import { auth } from "../service/firebase/firebaseConfig";
 
 interface AuthContextType {
   user: User | null;
+  loading: boolean;
 }
 
-const AuthContext = createContext<AuthContextType>({ user: null });
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  loading: true,
+});
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
       setUser(firebaseUser);
+      setLoading(false);
     });
 
     return () => unsubscribe();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, loading }}>
+      {!loading && children}
+    </AuthContext.Provider>
   );
 }
 
